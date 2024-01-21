3f2ceefaedebc985fec708aa9d31b4cb404b64a9
GARY <108233340+GARY121github@users.noreply.github.com>
2024-01-22 03:00:33 +0530

    healthcheck controller created

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -5,6 +5,15 @@ import { asyncHandler } from "../utils/asyncHandler.js"
 
 const healthcheck = asyncHandler(async (req, res) => {
     //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    return res.
+        status(201).
+        json(new ApiResponse(
+            201,
+            {
+                message: "Server Started Successfully",
+            },
+            "OK"
+        ))
 })
 
 export {
