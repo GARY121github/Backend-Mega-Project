7e51637f59cf3035e099e4cc0ace802b0ff65cb9
GARY <108233340+GARY121github@users.noreply.github.com>
2024-02-07 16:42:31 +0530

    Users-Subscribed Pipeline updated

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -98,53 +98,58 @@ const getUserChannelSubscribers = asyncHandler(async (req, res) => {
 
 // controller to return channel list to which user has subscribed
 const getSubscribedChannels = asyncHandler(async (req, res) => {
-    const { subscriberId } = req.params;
-    if (!isValidObjectId(subscriberId)) {
+    const { channelId } = req.params;
+    if (!isValidObjectId(channelId)) {
         throw new ApiError(400, "Invalid subscriber id");
     }
 
-    const user = await User.findById(subscriberId);
+    const user = await User.findById(channelId);
     if (!user) {
         throw new ApiError(404, "User not found while fetching subscribed channels");
     }
 
     const subscribedChannels = await Subscription.aggregate([
-        {
-            $match: {
-                subscriber: new mongoose.Types.ObjectId(subscriberId)
-            }
-        },
-        {
-            $lookup: {
-                from: "users",
-                localField: "channel",
-                foreignField: "_id",
-                as: "subscribedChannel"
-            }
-        },
-        {
-            $unwind: "$subscribedChannel"
-        },
-        {
-            $project: {
-                _id: 0,
-                channel: {
-                    _id: 1,
-                    name: 1,
-                    email: 1,
-                    avatar: 1
+            {
+                $match: {
+                    subscriber: new mongoose.Types.ObjectId(channelId)
+                }
+            },
+            {
+                $lookup: {
+                    from: "users",
+                    localField: "channel",
+                    foreignField: "_id",
+                    as: "subscribedChannel"
+                }
+            },
+            {
+                $unwind: "$subscribedChannel"
+            },
+            {
+                $project: {
+                    _id: 0,
+                    subscriber: 0,
+                    channel: 0,
+                    updatedAt: 0,
+                    __v: 0,
+                    createdAt: 0,
+                    subscribedChannel: {
+                        __v: 0,
+                        refreshToken: 0,
+                        password: 0,
+                        watchHistory: 0,
+                        createdAt: 0,
+                        updatedAt: 0
+                    }
                 }
             }
-        }
-    ]);
-
-    console.log(subscribedChannels);
+        ]);
 
     return res.
         status(201).
         json(new ApiResponse(
             201,
-            subscribedChannels = subscribedChannels[0],
+            subscribedChannels,
             "Subscribed channels fetched successfully"
         ));
 })
