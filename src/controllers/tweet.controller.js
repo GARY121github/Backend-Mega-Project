8c60c22ce4e556473d91aa975e881f49c712d8ed
GARY <108233340+GARY121github@users.noreply.github.com>
2024-02-15 11:50:11 +0530

    updated tweet controller

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -6,8 +6,6 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const createTweet = asyncHandler(async (req, res) => {
-    //TODO: create tweet
-
     const { content } = req.body;
 
     if (!content.trim()) {
@@ -36,16 +34,36 @@ const getUserTweets = asyncHandler(async (req, res) => {
         throw new ApiError(404, "User not found");
     }
 
-    // TODO: get user tweets
     const allTweets = await Tweet.aggregate([
         {
             $match: {
                 owner: new mongoose.Types.ObjectId(user._id)
+            },
+        },
+        {
+            $lookup: {
+                from: "users",
+                localField: "owner",
+                foreignField: "_id",
+                as: "owner"
+            }
+        },
+        {
+            $unwind: "$owner"
+        },
+        {
+            $project: {
+                "owner.password": 0,
+                "owner.email": 0,
+                "owner.createdAt": 0,
+                "owner.updatedAt": 0,
+                "owner.__v": 0,
+                "owner.refreshToken": 0,
+                "owner.coverImage": 0,
+                "owner.watchHistory": 0,
             }
         }
     ]);
-
-    console.log(allTweets);
 
     return res.status(200).json(new ApiResponse(
         200,
@@ -58,7 +76,7 @@ const updateTweet = asyncHandler(async (req, res) => {
     const { tweetId } = req.params;
     const { content } = req.body;
 
-    const tweet = await Tweets.findById(tweetId);
+    const tweet = await Tweet.findById(tweetId);
 
     if (!tweet) {
         throw new ApiError(404, "Tweet not found");
@@ -84,9 +102,7 @@ const updateTweet = asyncHandler(async (req, res) => {
 })
 
 const deleteTweet = asyncHandler(async (req, res) => {
-    //TODO: delete tweet
     const { tweetId } = req.params;
-
     const tweet = await Tweet.findById(tweetId);
 
     if (!tweet) {
@@ -97,7 +113,7 @@ const deleteTweet = asyncHandler(async (req, res) => {
         throw new ApiError(403, "You are not authorized to perform this action");
     }
 
-    await tweet.remove();
+    await Tweet.findByIdAndDelete(tweetId);
 
     return res.status(200).json(new ApiResponse(
         200,
