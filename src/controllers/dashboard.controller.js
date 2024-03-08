f64bc4f3103d27e04e25f0d52f3368988cf71f12
GARY <108233340+GARY121github@users.noreply.github.com>
2024-03-09 01:27:25 +0530

    Fixed the bug of get users dashboard API

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -11,7 +11,9 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
     const totalSubscribers = await Subscription.aggregate([
         {
-            $match: { channel: new mongoose.Types.ObjectId(userId) }
+            $match: {
+                channel: new mongoose.Types.ObjectId(userId)
+            }
         },
         {
             $count: 'totalSubscribers'
@@ -20,7 +22,9 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
     const totalVideosViews = await Video.aggregate([
         {
-            $match: { user: new mongoose.Types.ObjectId(userId) }
+            $match: {
+                owner: new mongoose.Types.ObjectId(userId)
+            }
         },
         {
             $group: {
@@ -32,19 +36,15 @@ const getChannelStats = asyncHandler(async (req, res) => {
 
     const totalLikes = await Like.aggregate([
         {
-            $match: { user: new mongoose.Types.ObjectId(userId) }
+            $match: {
+                likedBy: new mongoose.Types.ObjectId(userId)
+            }
         },
         {
-            $count: 'totalLikes'
-        }
-    ]);
-
-    const totalVideos = await Video.aggregate([
-        {
-            $match: { user: new mongoose.Types.ObjectId(userId) }
-        },
-        {
-            $count: 'totalVideos'
+            $group: {
+                _id: null,
+                totalLikes: { $sum: 1 } // Count the documents
+            }
         }
     ]);
 
@@ -52,7 +52,6 @@ const getChannelStats = asyncHandler(async (req, res) => {
         totalSubscribers: totalSubscribers[0]?.totalSubscribers || 0,
         totalVideosViews: totalVideosViews[0]?.totalViews || 0,
         totalLikes: totalLikes[0]?.totalLikes || 0,
-        totalVideos: totalVideos[0]?.totalVideos || 0
     }
 
     return res
@@ -94,7 +93,7 @@ const getChannelVideos = asyncHandler(async (req, res) => {
             }
         }
     ]);
-    
+
     return res
         .status(200)
         .json(new ApiResponse(
