6bc7e0a5c13927b37e669a48301e18ea2ef32ec9
GARY <108233340+GARY121github@users.noreply.github.com>
2024-03-12 05:02:57 +0530

    fixed imported bug

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import { fileURLToPath } from 'url';
+import path from 'path'; // Import the path module to work with file paths
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -14,26 +19,29 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static('public'));
 app.use(cookieParser());
 
-//routes import
-import userRouter from './routes/user.routes.js'
-import tweetRouter from "./routes/tweet.routes.js"
-import subscriptionRouter from "./routes/subscription.routes.js"
-import videoRouter from "./routes/video.routes.js"
-import commentRouter from "./routes/comment.routes.js"
-import likeRouter from "./routes/like.routes.js"
-import playlistRouter from "./routes/playlist.routes.js"
-import dashboardRouter from "./routes/dashboard.routes.js"
-import healthcheckRouter from "./routes/healthcheck.routes.js"
+// Define the directory where the routes are located
+const routesDir = path.join(__dirname, 'routes'); // Get the absolute path to the routes directory
+
+// Import routes using import
+import userRouter from './routes/user.routes.js';
+import tweetRouter from './routes/tweet.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import videoRouter from './routes/video.routes.js';
+import commentRouter from './routes/comment.routes.js';
+import likeRouter from './routes/like.routes.js';
+import playlistRouter from './routes/playlist.routes.js';
+import dashboardRouter from './routes/dashboard.routes.js';
+import healthcheckRouter from './routes/healthcheck.routes.js';
 
 //routes declaration
-app.use("/api/v1/users", userRouter)
-app.use("/api/v1/tweets", tweetRouter)
-app.use("/api/v1/subscriptions", subscriptionRouter)
-app.use("/api/v1/videos", videoRouter)
-app.use("/api/v1/comments", commentRouter)
-app.use("/api/v1/likes", likeRouter)
-app.use("/api/v1/playlist", playlistRouter)
-app.use("/api/v1/dashboard", dashboardRouter)
-app.use("/api/v1/healthcheck", healthcheckRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/tweets", tweetRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
+app.use("/api/v1/videos", videoRouter);
+app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
+app.use("/api/v1/healthcheck", healthcheckRouter);
 
 export default app;
