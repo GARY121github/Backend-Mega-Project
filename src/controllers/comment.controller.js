5f8de7f6f599c6dbc513d2b5711b4a1e17b633e4
GARY <108233340+GARY121github@users.noreply.github.com>
2024-03-15 01:38:41 +0530

    updated get video comments pipeline

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -6,9 +6,8 @@ import { ApiResponse } from "../utils/ApiResponse.js"
 import { asyncHandler } from "../utils/asyncHandler.js"
 
 const getVideoComments = asyncHandler(async (req, res) => {
-    //TODO: get all comments for a video
-    const { videoId } = req.params
-    const { page = 1, limit = 10 } = req.query
+    const { videoId } = req.params;
+    const { page = 1, limit = 10 } = req.query;
 
     const video = await Video.findById(videoId);
 
@@ -31,19 +30,37 @@ const getVideoComments = asyncHandler(async (req, res) => {
         {
             $limit: parseInt(limit),
         },
+        {
+            $lookup: {
+                from: "users", // Assuming the User collection name is 'users'
+                localField: "owner",
+                foreignField: "_id",
+                as: "user",
+            },
+        },
+        {
+            $unwind: "$user", // Unwind the array created by $lookup
+        },
+        {
+            $project: {
+                _id: 1,
+                content: 1,
+                createdAt: 1,
+                user: { _id: 1, username: 1, email: 1 , avatar: 1 }, // Select user fields you want to include
+            },
+        },
     ];
 
-    const result = await Comment.aggregatePaginate(aggregateOptions);
+    const comments = await Comment.aggregate(aggregateOptions);
 
-    return res.
-        status(201).
-        json(new ApiResponse(
-            201,
-            result,
+    return res
+        .status(200)
+        .json(new ApiResponse(
+            200,
+            comments,
             "Comments fetched successfully"
-        ))
-
-})
+        ));
+});
 
 const addComment = asyncHandler(async (req, res) => {
     const { videoId } = req.params;
